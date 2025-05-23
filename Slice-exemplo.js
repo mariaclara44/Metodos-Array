fd8ba23d12d83675d0fd1da7f41edf056929980c
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-05-23 10:02:42 -0300

    Modifca o arquivo Slice-exemplo na pasta Metodos-Array

--- Slice-exemplo.js
+++ Slice-exemplo.js
@@ -4,3 +4,4 @@ console.log(`Parte da playlist (índices 2 a 5): ${playlist.slice(2, 6)}`); // [
 
 console.log(`Parte da playlist (Índices 0 a 3): ${playlist.slice(0, 4)}`); //['Funk', 'Hip-Hop', 'Pop', 'Rock']
 
+console.log(`Parte da playlist (índice 3 a 4): ${playlist.slice (3, 4)}`);
