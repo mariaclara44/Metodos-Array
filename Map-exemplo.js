40ff71d83e7dce4c33b8927a1e9d39b5fef69a52
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-05-23 11:18:59 -0300

    Modifica o arquivo Map-exemplo na pasta Metodos-Array

--- Map-exemplo.js
+++ Map-exemplo.js
@@ -4,3 +4,22 @@ let playlistEmCaps = playlist.map(musica => musica.toUpperCase());
 
 console.log(`Playlist em maiúsculas: ${playlistEmCaps}`);
 
+let clone = playlist.map (item => item); // apenas copia os itens
+
+console.log(`Clonagem com map(): ${clone.join(', ')}`);
+
+
+let minuscula = playlist.map(item => item.toLowerCase());
+console.log(`Minúsculas com map(): ${minuscula.join(', ')}`);
+
+let numeros = [10, 20, 30, 40, 50];
+
+let dobrados = numeros.map(n=> n * 2);
+console.log(`Valores dobrados: ${dobrados.join(', ')}`);
+
+let metades = numeros.map(n => n / 2);
+console.log(`Valores pela metade: ${metades.join(', ')}`);
+
+let comoTexto = numeros.map(n => `${n} pts`);
+console.log(`Como texto ${comoTexto.join(', ')}`);
+
