4c3a92a4ae614445ee91a0c33ed40a918a67ed85
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-05-23 09:31:09 -0300

    Adicona arquivo Includes-exemplo na pasta Metodos-Array

---
+++ Includes-exemplo.js
@@ -0,0 +1,5 @@
+let playlist = ["Funk", "Hip-Hop", "Pop", "Opera", " Hard-Rock", "Ninar", "Jazz"];
+
+
+console.log(`A playlist inclui a palavra Funk? ${playlist.includes("Funk")}`);
+console.log(`A playlist inclui a palavra Rock? ${playlist.includes("Rock")}`);
